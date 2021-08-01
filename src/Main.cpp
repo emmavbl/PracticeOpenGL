a26b831ef0fe59fa6127cc4d956ebabcbc1e620f
evodable <vodableemma@gmail.com>
2021-08-01 17:10:21 +0200

    Draw a Triangle

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,6 +8,24 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_HEIGHT = 600;
 const unsigned int SCR_WIDTH = 1200;
 
+const char* vertexShaderSource = 
+"#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource =
+"#version 330 core\n"
+"out vec4 FragColor;\n"
+
+"void main()"
+"{\n"
+"    FragColor = vec4(0.866f, 0.537f, 0.211f, 1.0f);\n"
+"}\0";
+
+
 int main(void)
 {
     glfwInit();
@@ -20,15 +38,83 @@ int main(void)
         return -1;
     }
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // Load OpenGL function with GLAD
+    // Load OpenGL functions with GLAD
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    // Generate vertex shader object
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    // Attach glsl code to the shader and compile it
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // Check Vertex shader compilation sucess
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Generate fragment shader object
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // Check fragment shader compilation sucess
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Create Program
+    unsigned int shaderProgram = glCreateProgram();
+    // Attach vertex and fragment shader
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // Check shader program linking
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Must delete shaders once compiled and linked in the shader program
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+
+    // Generate VBO and VAO 
+    unsigned int VBO; 
+    unsigned int VAO;
+    glGenBuffers(1, &VBO);
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);// Bind VAO
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);// Bind VBO
+            // Load vertices' data to the VBO
+            glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+            // Configure OpenGl's vertices reading in VBO
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+            glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0); // Unbind VBO
+    glBindVertexArray(0); // Unbind VAO
+
+    // Draw in Wireframe
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
 
     // Render Loop
     while (!glfwWindowShouldClose(window))
@@ -37,8 +123,15 @@ int main(void)
         processInput(window);
         
         // Rendering command
-        glClearColor(0.337f, 0.137f, 0.482f, 1.0f);
+        // Clear
+        glClearColor(0.529f, 0.850f, 0.776f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        // Draw
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glBindVertexArray(0); // no need to unbind it every time 
 
         // Check events and swap buffers
         glfwSwapBuffers(window);
